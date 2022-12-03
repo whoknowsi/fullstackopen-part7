56a7ccc11058938d5788e42315631bd0f081cadd
whoknowsi <echosmania@gmail.com>
2022-12-03 02:51:51 -0300

    7.6 - Fixed error caused by object spread

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -55,9 +55,9 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const content = useField('content')
-  const author = useField('author')
-  const info = useField('info')
+  const { reset: resetContent, ...content } = useField('content')
+  const { reset: resetAuthor, ...author } = useField('author')
+  const { reset: resetInfo, ...info } = useField('info')
 
   const navigate = useNavigate()
 
@@ -75,9 +75,9 @@ const CreateNew = (props) => {
   }
 
   const handleReset = () => {
-    content.reset()
-    author.reset()
-    info.reset()
+    resetContent()
+    resetAuthor()
+    resetInfo()
   }
 
   return (
