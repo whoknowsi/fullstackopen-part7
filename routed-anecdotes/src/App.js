b597c56a602c25bc4b8314af59e0a2c4dbe13c6b
whoknowsi <echosmania@gmail.com>
2022-12-03 02:34:17 -0300

    7.5 - Added reset button to new anecdote form expanding useField custom hook

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -74,6 +74,12 @@ const CreateNew = (props) => {
     setTimeout(() => props.setNotification(''), 10000)
   }
 
+  const handleReset = () => {
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -90,7 +96,7 @@ const CreateNew = (props) => {
           url for more info
           <input {...info} />
         </div>
-        <button>create</button>
+        <button type='submit'>create</button><button type='button' onClick={handleReset}>reset</button>
       </form>
     </div>
   )
